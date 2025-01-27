c96d097c037382f6d1cf088f770b592e9812ef12
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-01-27 14:32:37 +0100

    test video named properly

--- backend/crosswalk-detection-model/test.py
+++ backend/crosswalk-detection-model/test.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
-# Load a pretrained YOLO11n model
 model = YOLO("best.pt")
 
-# Run inference on 'bus.jpg' with arguments
-model.predict("videoplayback.mp4", imgsz=640,show=True )
+model.predict("test2.mp4", imgsz=640,show=True )
