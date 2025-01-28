c6e99e9ad19ef8e2fd65a76b8d857217edf07d00
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-01-28 14:56:12 +0100

    allow all hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['50d0-193-225-218-240.ngrok-free.app']
+    allowedHosts: true
   }
 })
