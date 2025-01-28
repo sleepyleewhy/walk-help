81440a4442871fa51724a7fb34592f051092a4ce
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-01-28 12:21:20 +0100

    restructuring

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-import base64
-from ultralytics import YOLO
-import numpy as np
-import cv2
 from fastapi.middleware.cors import CORSMiddleware
 
 from sockets import sio_app
-
-
-def base64_to_image(base64_string):
-    img_bytes = base64.b64decode(base64_string)
-    img_array = np.frombuffer(img_bytes, dtype=np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    print(img)
-    return img
-
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
@@ -22,7 +9,7 @@ app.add_middleware(CORSMiddleware,
                 allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"])
+
 app.mount('/', app=sio_app)
 
-model = YOLO("./crosswalk-detection-model/best.pt")
 
