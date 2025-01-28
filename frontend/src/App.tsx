a5bc4f8fc74099dd5fa23993c001b49ed90c0a4c
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-01-28 14:57:48 +0100

    app tsx setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 
 import './App.css'
 import Accelerometer from './Accelerometer'
-import ImageCapture from './ImageCapture'
+import CrosswalkDetection from './CrosswalkDetection'
+import { useState } from 'react'
+import { io } from 'socket.io-client'
 
 function App() {
+  const [isCurrWatching, setIsCurrWatching] = useState(false)
 
+  const socket = io('http://127.0.0.1:8000', {
+    path: '/sockets',
+    transports: ['websocket'],
+  });
   return (
     <>
       <div>
-        <Accelerometer/>
-        <ImageCapture/>
+        <Accelerometer isCurrWatching={isCurrWatching} setIsCurrWatching={setIsCurrWatching}/>
+        <CrosswalkDetection isWatching={isCurrWatching} socket={socket}/>
       </div>
     </>
   )
