2cd964b38c4ce97005d8c9506f7a130d4bc7b3c3
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-01-28 13:19:01 +0100

    frontend initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Accelerometer from './Accelerometer'
+import ImageCapture from './ImageCapture'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Accelerometer/>
+        <ImageCapture/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['50d0-193-225-218-240.ngrok-free.app']
+  }
+})
