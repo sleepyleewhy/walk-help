9986b6a27428012005e15ca2b5803192310d2df3
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-03-12 09:26:30 +0100

    added routes to app.tsx, installed react-router-dom, moved sounds to assets folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,31 @@
 
-import './App.css'
 import Accelerometer from './Accelerometer'
-import CrosswalkDetection from './CrosswalkDetection'
+import './App.css'
+// import CrosswalkDetection from './CrosswalkDetection'
 import { useState } from 'react'
-import { io } from 'socket.io-client'
+// import { io } from 'socket.io-client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [isCurrWatching, setIsCurrWatching] = useState(false)
 
-  const socket = io('http://127.0.0.1:8000', {
-    path: '/sockets',
-    transports: ['websocket'],
-  });
+  // const socket = io('http://127.0.0.1:8000', {
+  //   path: '/sockets',
+  //   transports: ['websocket'],
+  // });
   return (
-    <>
-      <div>
-        <Accelerometer isCurrWatching={isCurrWatching} setIsCurrWatching={setIsCurrWatching}/>
-        <CrosswalkDetection isWatching={isCurrWatching} socket={socket}/>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accelerometer isCurrWatching={isCurrWatching} setIsCurrWatching={setIsCurrWatching} />} />
+      </Routes>
+    </BrowserRouter>
+
+    // <>
+    //   <div>
+    //     <Accelerometer isCurrWatching={isCurrWatching} setIsCurrWatching={setIsCurrWatching}/>
+    //     <CrosswalkDetection isWatching={isCurrWatching} socket={socket}/>
+    //   </div>
+    // </>
   )
 }
 
