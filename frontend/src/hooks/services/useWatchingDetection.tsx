e85ecedcf4023a7781ba35e1b9163e7b79804035
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-03-28 13:40:09 +0100

    uses the average of the last 50 values to determine the current magnitude

--- frontend/src/hooks/services/useWatchingDetection.tsx
+++ frontend/src/hooks/services/useWatchingDetection.tsx
@@ -3,50 +3,59 @@ import { SetStateAction, useEffect, useRef, useState } from "react";
 
 
 
-const useWatchingDetection = (magnitude : number, isMagnitudeActive : boolean, setIsMagnitudeActive : React.Dispatch<SetStateAction<boolean>>, 
+const useWatchingDetection = (magnitude: number, isMagnitudeActive: boolean, setIsMagnitudeActive: React.Dispatch<SetStateAction<boolean>>,
     magnitudeThreshold: number, alertLevel: number, setAlertLevel: React.Dispatch<SetStateAction<number>>
 ) => {
 
     const [isWatchingDetectionActive, setIsWatchingDetectionActive] = useState<boolean>(false);
-    const lowMagnitudeCounter = useRef(0);
-    const highMagnitudeCounter = useRef(0);
-
+    const [magnitudeAverage, setMagnitudeAverage] = useState(0);
+    const magnitudeHistory = useRef<number[]>([]);
 
     useEffect(() => {
-        if (alertLevel >= 0){
+        if (isWatchingDetectionActive && isMagnitudeActive) {
+            magnitudeHistory.current.push(magnitude);
+            if (magnitudeHistory.current.length > 50) {
+                magnitudeHistory.current.shift();
+            }
+            if (magnitudeHistory.current.length > 0) {
+                setMagnitudeAverage(magnitudeHistory.current.reduce((sum, value) => sum + value, 0) / magnitudeHistory.current.length);
+            }
+            else {
+                setMagnitudeAverage(0);
+            }
+
+        }
+
+    }, [magnitude, isWatchingDetectionActive, isMagnitudeActive]);
+
+    useEffect(() => {
+
+    })
+
+    useEffect(() => {
+        if (alertLevel >= 0) {
             setIsWatchingDetectionActive(true);
-            if (!isMagnitudeActive){
+            if (!isMagnitudeActive) {
                 setIsMagnitudeActive(true);
             }
-            if (magnitude > magnitudeThreshold){
-                lowMagnitudeCounter.current = 0;
-                highMagnitudeCounter.current++;
-                if (highMagnitudeCounter.current >= 10){
-                    if (alertLevel === 0){
-                        setAlertLevel(1);
-                    }
-                    highMagnitudeCounter.current = 0;
+            if (magnitudeAverage > magnitudeThreshold) {
+
+                if (alertLevel < 1) {
+                    setAlertLevel(1);
                 }
             }
             else {
-                highMagnitudeCounter.current = 0;
-                lowMagnitudeCounter.current++;
-                if (lowMagnitudeCounter.current >= 10){
-                    if (alertLevel >= 1){
-                        setAlertLevel(0);
-                    }
-                    lowMagnitudeCounter.current = 0;
+                if (alertLevel >= 1) {
+                    setAlertLevel(0);
                 }
             }
-
-
         }
-    }, [alertLevel, isMagnitudeActive, magnitude, magnitudeThreshold, setAlertLevel, setIsMagnitudeActive] )
+    }, [alertLevel, isMagnitudeActive, magnitudeThreshold, setAlertLevel, setIsMagnitudeActive, magnitudeAverage])
 
 
 
 
-    return isWatchingDetectionActive;
+return isWatchingDetectionActive;
 }
 
 
