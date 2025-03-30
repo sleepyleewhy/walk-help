64385d6d0095b6545138f716ee9dbd081bb919d3
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-03-30 21:05:29 +0200

    refactoring

--- frontend/src/hooks/sensors/useCamera.tsx
+++ frontend/src/hooks/sensors/useCamera.tsx
@@ -2,67 +2,77 @@ import { useEffect, useRef, useState } from "react";
 
 
 
-const useCamera = (fps : number = 30) => {
-    const [camera, setCamera] = useState<MediaStream | null>(null);
+const useCamera = (fps: number = 10) => {
+    const camera = useRef<MediaStream | null>(null);
     const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
     const [imageAsBase64, setImageAsBase64] = useState<string>("");
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-
-
-    useEffect(() => {
-        const getCameraStream = async () => {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({
-                    video: {
-                        facingMode: 'environment',
-                        width: { ideal: 640 },
-                        height: { ideal: 640}
-                    },
-                });
-                setCamera(stream);
-            }catch (err) {
-                console.error('Error accessing camera:', err);
+    const intervalId = useRef<number | null >(null);
+    const getCameraStream = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                    facingMode: 'environment',
+                    width: { ideal: 640 },
+                    height: { ideal: 640 }
+                },
+            });
+            camera.current = stream;
+            if (videoRef.current) {
+                videoRef.current.srcObject = stream;
             }
-        };
-        if (isCameraActive){
-            getCameraStream();
-        } else {
-                setCamera(null);
+            console.log('Camera stream:', stream);
+        } catch (err) {
+            console.error('Error accessing camera:', err);
         }
+    };
+
+    const captureImage = () => {
+        if (canvasRef.current && videoRef.current) {
+            const ctx = canvasRef.current.getContext('2d');
+
+            if (ctx) {
+                ctx.drawImage(videoRef.current, 0, 0, 640, 640);
+                setImageAsBase64(canvasRef.current.toDataURL('image/jpeg'));
+            }
+        }
+    }
+    useEffect(() => {
+        if (isCameraActive) {
+            getCameraStream();
+            intervalId.current = window.setInterval(captureImage, 1000 / fps);
+
+        } else {
+            if (camera.current){
+                camera.current?.getTracks().forEach(track => track.stop());
+                camera.current = null;
+            }
+
+            if (videoRef.current) {
+                videoRef.current.srcObject = null;
+            }
+            if (intervalId.current) {
+                clearInterval(intervalId.current);
+                intervalId.current = null;
+            }
+        }
+
+
+
 
         return () => {
-            if (camera) {
-                camera.getTracks().forEach(track => {track.stop();});
+            if (intervalId.current) {
+                clearInterval(intervalId.current);
             }
-
-        }
-    }, [isCameraActive, camera]);
-
-    useEffect(() => {
-        if (camera && videoRef.current && canvasRef.current) {
-            videoRef.current.srcObject = camera;
-        }
-
-        const captureImage = () => {
-            const canvas = canvasRef.current;
-            const video = videoRef.current;
-            if (canvas && video){
-                const ctx = canvas.getContext('2d');
-                if (ctx){
-                    ctx.drawImage(video, 0, 0, 640, 640);
-                    setImageAsBase64(canvas.toDataURL('image/jpeg'));
-                }
+            if (camera.current) {
+                camera.current.getTracks().forEach(track => { track.stop(); });
             }
         }
+    }, [isCameraActive, fps]);
 
-        const intervalId = setInterval(captureImage, 1000/ fps);
 
-        return () => { clearInterval(intervalId); };
-
-    }, [camera, fps]);
-    
-    return {isCameraActive, setIsCameraActive, imageAsBase64, videoRef, canvasRef};
+    return { isCameraActive, setIsCameraActive, imageAsBase64, videoRef, canvasRef };
 }
 
 export default useCamera;
--- frontend/src/hooks/services/useCrosswalkDetection.tsx
+++ frontend/src/hooks/services/useCrosswalkDetection.tsx
@@ -10,14 +10,17 @@ const useCrosswalkDetection = (socket: Socket, imageAsBase64: string, alertLevel
     const isCrosswalkDetectionActive = alertLevel >= 1;
     const noCrosswalkCounter = useRef(0);
     const username = localStorage.getItem("username");
-    const intervalId = useRef<NodeJS.Timeout | number>(-1);
-    useEffect(() => {
+    const intervalId = useRef<number | null>(null);
+    const imageRef = useRef(imageAsBase64);
 
-        if (alertLevel >= 1) {
-            if (!isCameraActive) {
-                setIsCameraActive(true);
-            }
-            socket.on("predict_result_" + username, (data) => {
+    useEffect(() => {
+        imageRef.current = imageAsBase64;
+    }, [imageAsBase64]);
+
+
+    useEffect(() => {
+        socket.on("predict_result_" + username, (data) => {
+            if (alertLevel >=1){
                 if (data === true) {
                     noCrosswalkCounter.current = 0;
                     if (alertLevel === 1) {
@@ -33,18 +36,36 @@ const useCrosswalkDetection = (socket: Socket, imageAsBase64: string, alertLevel
                         noCrosswalkCounter.current++;
                     }
                 }
-            });
-            intervalId.current = setInterval(() => {
-                socket.emit("predict", username, imageAsBase64);
-                console.log('predict', imageAsBase64);
-            }, 100);
+            }
+
+        });
+    },[socket, alertLevel, username, setAlertLevel] )
+
+
+    useEffect(() => {
+        if (alertLevel >= 1) {
+            if (!isCameraActive) {
+                setIsCameraActive(true);
+            }
+            if (intervalId.current == null) {
+                intervalId.current = window.setInterval(() => {
+
+                    if (imageRef.current != "") {
+                        socket.emit("predict", username, imageRef.current);
+                    }
+                }, 1000 / 5);
+            }
+            
         }
         else {
             if (isCameraActive) {
                 setIsCameraActive(false);
             }
             socket.off("predict_result_" + username);
-            clearInterval(intervalId.current);
+            if (intervalId.current) {
+                clearInterval(intervalId.current);
+                intervalId.current = null;
+            }
         }
 
 
